b498186db4afe11fd5453f0ace5af1ab39f315c2
cmcharnley <49981517+cmcharnley@users.noreply.github.com>
2019-06-07 13:46:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-function rollDice () {
-    let diceRoll = (Math.floor(Math.random()*6+1));
-    return diceRoll; 
+let randomDiceRoll;
+let computerRoll;
+let score = 0;
+let computerScore = 0; 
+
+function takeTurn() {
+
+    randomDiceRoll = (Math.floor(Math.random()*6+1));
+
+    score = score + randomDiceRoll;
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("diceImage").src = `img/dice${randomDiceRoll}.png`;
 }
 
-let randomDiceRoll;
 
-let score = 0;
+function computerTakeTurn() {
 
-document.getElementById("button").addEventListener("click", function(){
-    randomDiceRoll = rollDice();
-    score = score + randomDiceRoll;
-    document.getElementById("score").innerHTML = score;
-    document.getElementById("diceImage").src = `img/dice${randomDiceRoll}.png`
-})
+    computerRoll = (Math.floor(Math.random()*6+1));
+
+    computerScore = computerScore + computerRoll;
+
+    document.getElementById("computerScore").innerHTML = computerScore;
+    document.getElementById("diceImage").src = `img/dice${computerRoll}.png`;
+}
+
+document.getElementById("button").addEventListener("click", takeTurn); 
